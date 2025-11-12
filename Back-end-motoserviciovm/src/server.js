1940cd3b670fa0fd74236c06fc29d4fc5df8965d
KEVIN01306 <kevin60sanchez31@gmail.com>
2025-11-11 19:54:29 -0600

    commit dominios para el back

--- Back-end-motoserviciovm/src/server.js
+++ Back-end-motoserviciovm/src/server.js
@@ -13,7 +13,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.use(cors({
-    origin: '*'
+    origin: ['https://motoserviciovm.com', 'https://www.motoserviciovm.com']
 }))
 
 app.use("/",routes);
