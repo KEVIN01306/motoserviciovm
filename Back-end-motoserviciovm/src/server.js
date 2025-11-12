5e1708e3dc4f91c773fcc902a775e24ff95be291
KEVIN01306 <kevin60sanchez31@gmail.com>
2025-11-12 12:24:53 -0600

    Update server.js

--- Back-end-motoserviciovm/src/server.js
+++ Back-end-motoserviciovm/src/server.js
@@ -13,7 +13,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.use(cors({
-    origin: ['motoserviciovm.com', 'www.motoserviciovm.com']
+    origin: '*'
 }))
 
 app.use("/",routes);
