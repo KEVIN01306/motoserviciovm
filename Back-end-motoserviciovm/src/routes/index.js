b993d4e9d5f9efcf13f4ece865da680d7d3eba02
KEVIN01306 <kevin60sanchez31@gmail.com>
2025-11-15 13:43:05 -0600

    Update index.js

--- Back-end-motoserviciovm/src/routes/index.js
+++ Back-end-motoserviciovm/src/routes/index.js
@@ -5,6 +5,6 @@ const router = Router();
 router.use('/users', (await import('./user.routes.js')).default);
 router.use('/roles', (await import('./rol.routes.js')).default);
 router.use('/permisos', (await import('./permiso.routes.js')).default);
-router.use('/auth', (await import('./Auth.routes.js')).default);
+router.use('/auth', (await import('./auth.routes.js')).default);
 
 export default router;
