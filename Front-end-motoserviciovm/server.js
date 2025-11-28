c30426099c3499996eff04bc591cef999ddb9438
KEVIN01306 <kevin60sanchez31@gmail.com>
2025-11-27 18:21:19 -0600

    commit

--- Front-end-motoserviciovm/server.js
+++
@@ -1,3 +0,0 @@
-// server.js removed: proxy was added temporarily and has been reverted per user request.
-// If you need to re-enable an AI proxy later, recreate this file with your server logic.
-
