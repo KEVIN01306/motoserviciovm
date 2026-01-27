f1e3ed7e0e98aac9a062d7acc006f6a8b340d80a
KEVIN01306 <kevin60sanchez31@gmail.com>
2026-01-27 17:37:06 -0600

    Update vite.config.ts

--- Front-end-motoserviciovm/vite.config.ts
+++ Front-end-motoserviciovm/vite.config.ts
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [tailwindcss(),react()],
   server: {
-    host: true,
+    host: false,
     allowedHosts: ['motoserviciovm.com', 'www.motoserviciovm.com'],
     port: 4001,
   },
